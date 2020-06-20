4b73c1edf482220d9355bb39c46fe820ea0febb2
Nello Carotenuto <nello.carotenuto.96@live.com>
2020-06-20 16:39:19 +0200

    Preprocessing: define a module to add thresholds to graph edges

---
+++ src/preprocessing/edge_thresholds.py
@@ -0,0 +1,39 @@
+import random
+import snap
+
+
+def set_random(graph, seed):
+    """Sets a random activation threshold for every edge of the graph."""
+
+    # Ensure attributes can be set on the graph
+    if not isinstance(graph, snap.PNEANet):
+        raise Exception("Attributes can only be added to networks")
+
+    # Set the random seed to be able to reproduce results
+    random.seed(seed)
+
+    # Generate a random threshold for each edge in the graph and add it
+    for destination in graph.Nodes():
+        for source in destination.GetInEdges():
+            edge = graph.GetEI(source, destination.GetId())
+            threshold = random.uniform(0, 1)
+
+            graph.AddFltAttrDatE(edge, threshold, "threshold")
+
+
+def set_degree_proportional(graph):
+    """Sets a threshold proportional to the degree of the node."""
+
+    # Ensure attributes can be set on the graph
+    if not isinstance(graph, snap.PNEANet):
+        raise Exception("Attributes can only be added to networks")
+
+    # Compute the threshold based on the in-degree of each node and add it to its edges
+    for destination in graph.Nodes():
+        degree = destination.GetInDeg()
+        threshold = 1 / degree
+
+        for source in destination.GetInEdges():
+            edge = graph.GetEI(source, destination.GetId())
+            graph.AddFltAttrDatE(edge, threshold, "threshold")
+
